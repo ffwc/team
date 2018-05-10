04716dd98feff48d49b1f742031438033e16c154
âxiejie <17310225090@163.com>
2018-05-10 11:39:59 +0800

    添加界面

--- app/src/main/java/com/example/android/new_nds_study/fragment/LiveFragment.java
+++ app/src/main/java/com/example/android/new_nds_study/fragment/LiveFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,23 +32,35 @@ public class LiveFragment extends Fragment {
 
 
     private View view;
-    private ImageView mIvDetail;
+    private RelativeLayout mIvDetail;
     private ObservableScrollView mScrollView;
     private TextView mTvTitlebar;
     private RelativeLayout mLayoutTitle;
     private int mImageHeight;
+    private TextView textView_ste;
+    private int t;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.live_fragment, container, false);
 
-        mIvDetail = (ImageView) view.findViewById(R.id.iv_detail);
+        mIvDetail = (RelativeLayout) view.findViewById(R.id.iv_detail);
         mScrollView = (ObservableScrollView) view.findViewById(R.id.scrollView);
         mTvTitlebar = (TextView) view.findViewById(R.id.tv_titlebar);
         mLayoutTitle = (RelativeLayout) view.findViewById(R.id.layout_title);
+        textView_ste = view.findViewById(R.id.textview_set);
+        textView_ste.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ClassnameActivity.class);
+                startActivity(intent);
+            }
+        });
 
-        //获取顶部的图片高度
         initListener();
+        onScrollChanged(t);
+        mScrollView.setScrollViewListener();
+
         //设置ScrollVIew的滚动监听,一般滑动时,最上面的标题显示,参数是ScrollViewListener,我们让Activity去实现
         mScrollView.setScrollViewListener(getActivity());
         return view;
@@ -67,32 +80,34 @@ public class LiveFragment extends Fragment {
 //                mIvDetail.getViewTreeObserver().removeOnGlobalLayoutListener(this);
                 //得到控件的高度
                 mImageHeight = mIvDetail.getHeight();
+
             }
         });
     }
 
     //自定义ObservableScrollView滑动监听器,是ObservableScrollView在把图片滑消失后,显示出标题的效果
-    public void onScrollChanged(ObservableScrollView scrollView, int l, int t, int oldl, int oldt) {
+    public void onScrollChanged(int t) {
         //对T轴进行判断,就两种形态:1.消失没有   2.随着滑动,颜色越来越深
-        System.out.println("t"+t);
+        System.out.println("t"+ t);
+        Log.i("控件的高度", String.valueOf(mImageHeight));
         if (t <=0){
             //设置标题隐藏
-            mTvTitlebar.setVisibility(View.GONE);
+            mTvTitlebar.setVisibility(View.INVISIBLE);
             //设置标题所在背景为透明
-            mLayoutTitle.setBackgroundColor(Color.argb(0,0,0,0));
+            //mLayoutTitle.setBackgroundColor(Color.argb(0,0,0,0));
         }
-        else if(t>0 && t<mImageHeight){
+        else if(t >mImageHeight){
             //让标题显示出来
-            mTvTitlebar.setVisibility(View.VISIBLE);
+            mLayoutTitle.setVisibility(View.VISIBLE);
             //获取ScrollView向下滑动,图片消失部分的比例
-            float scale =(float) t / mImageHeight;
+            //float scale =(float) t / mImageHeight;
             //根据这个比例,让标题的颜色慢慢的由浅入深
-            float alpha = 255 * scale;
+           // float alpha = 255 * scale;
             //设置标题的内容及颜色
-            mTvTitlebar.setText("颜值担当是谁???");
-            mTvTitlebar.setTextColor(Color.argb((int)alpha,0,0,0));
+            mTvTitlebar.setText("本堂课的名称");
+            //mTvTitlebar.setTextColor(Color.argb((int)alpha,0,0,0));
             //设置标题布局颜色
-            mLayoutTitle.setBackgroundColor(Color.argb((int)alpha,255,255,255));
+           /// mLayoutTitle.setBackgroundColor(Color.argb((int)alpha,255,255,255));
         }
     }
 
