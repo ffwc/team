d9214399631110b55d1cd32d94bf084adc7247c2
fzw <407432877@qq.com>
2018-06-15 14:58:08 +0800

    test

---
+++ app/src/main/java/com/example/android/new_nds_study/m_v_p/view/TabLayoutPresenterListener.java
@@ -0,0 +1,12 @@
+package com.example.android.new_nds_study.m_v_p.view;
+
+import com.example.android.new_nds_study.m_v_p.bean.TabLayoutBean;
+
+/**
+ * @Author J & J
+ * @Time 2018/5/14.
+ */
+
+public interface TabLayoutPresenterListener {
+    void successe(TabLayoutBean tabLayoutBean);
+}
