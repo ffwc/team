ec29d84a6452539557d33a736e22be75e7ef3075
ChenKun-CK <2322477054@qq.com>
2018-06-25 19:24:37 +0800

    Merge branch 'sbb250' of https://github.com/ffwc/test into ck
    # Conflicts:
    #	.idea/modules.xml
    #	app/src/main/java/com/example/android/new_nds_study/fragment/eachfragment/LeaguerFragment.java

--- app/src/main/java/com/example/android/new_nds_study/fragment/eachfragment/QuizFragment.java
+++ app/src/main/java/com/example/android/new_nds_study/fragment/eachfragment/QuizFragment.java
@@ -1,6 +1,5 @@
 package com.example.android.new_nds_study.fragment.eachfragment;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,11 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.android.new_nds_study.MyApp;
 import com.example.android.new_nds_study.R;
-import com.example.android.new_nds_study.activity.LoginActivity;
 import com.example.android.new_nds_study.adapter.QuizAdapter;
 import com.example.android.new_nds_study.m_v_p.bean.QuizBean;
 import com.example.android.new_nds_study.m_v_p.presnster.ClassQuizPresenter;
